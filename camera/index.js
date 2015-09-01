b00327b189357c3059fcba69c1f206a40b7bc01b
DualMatrix <piet.goris@gmail.com>
2015-08-31 18:09:06 -0700

    Tweaked Camera

--- camera/index.js
+++ camera/index.js
@@ -1,13 +1,55 @@
+var Vector = require("../vector");
+var Rederer= requite("../draw-canvas");
 
-module.exports = {}
 var Camera = function(obj, D){
-	this.position = obj.Position;
-	this.offset = ;
-	this.dimensions = D;
+    this.active = true;
+	this.displaying = false;
+    this.position = obj.position;
+    this.dimensions = D;
+	this.offset = new Vector(0,0);
+	
+    return this;
 }
-Camera.prototype.reset = function(){
-	this.offset = ;
+Camera.prototype.activate = funtion(){
+    //Turns Camera On
+    if(this.active){
+        console.log("This camera is already active!");
+    }
+    else{        
+        this.active = true;
+    }
+}
+Camera.prototype.deactivate = function(){
+    //Turns Camera Off
+    if(!this.active){
+        console.log("This camera is already inactive!");
+    }
+    else{        
+        this.active = false;
+    }
+}
+Camera.prototype.linkTo = function(obj){
+    //Links Camera to gameObject
+    this.position = obj.position;
+}
+Camera.prototype.reset  = function(){
+	this.offset.reset();
 }
 Camera.prototype.move  = function(V){
-	
+	this.offset.add(V);
 }
+Camera.prototype.drawScene = function(scene){
+    //Draws the visible scene
+    if(this.active){
+        if(this.position.x - this.dimensions.x < 0){
+            this.position = this.dimensions.x;
+        }
+        if(this.position.y - this.dimensions.y < 0){
+            this.position = this.dimensions.y;
+        }
+        if(this.displaying){
+            //Draw Camera
+        }
+    }
+}
+module.exports = Camera;
--- vector/index.js
+++ vector/index.js
@@ -1,1 +1,8 @@
-module.exports = require("../colliders").Vector;
+var V = require("../colliders").Vector;
+V.prototype.reset = function(){
+	this.x = 0;
+	this.y = 0;
+
+	return this;
+}
+module.exports = V;
