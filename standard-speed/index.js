be13c49a17ae04abc689b29d13c4094d45aa659f
DualMatrix <piet.goris@gmail.com>
2015-09-05 17:14:48 -0700

    Moar Broken!!!!

--- standard-speed/index.js
+++ standard-speed/index.js
@@ -8,7 +8,7 @@ var standards = {
 events.on("loop:update:before", function(){
   //console.log(clock.deltaTime.update / 1000 * 60);
   //console.log(clock.deltaTime.update);
-  renderer.drawText(clock.deltaTime.update, "20px Georgia", new Vector(0,90));
+  renderer.drawText(clock.deltaTime.draw, "20px Georgia", new Vector(0,90));
 });
 
 module.exports = standards;
